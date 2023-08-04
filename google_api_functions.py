9acdfe1e35341fb4a9852a3518d5a2d988408c36
Nick <nbochen@gmail.com>
2023-08-04 12:38:16 -0400

    Version 1.0

--- google_api_functions.py
+++ google_api_functions.py
@@ -2,37 +2,77 @@ import os
 import pickle
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
+
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
+
 def get_credentials():
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+    creds = Credentials.from_service_account_file('visitoremail-data-28544584c945.json', scopes=SCOPES)
     return creds
 
+# def get_credentials(): Use Locally
+#     creds = None
+#     if os.path.exists('token.pickle'):
+#         with open('token.pickle', 'rb') as token:
+#             creds = pickle.load(token)
+#     if not creds or not creds.valid:
+#         if creds and creds.expired and creds.refresh_token:
+#             creds.refresh(Request())
+#         else:
+#             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+#             creds = flow.run_local_server(port=0)
+#         with open('token.pickle', 'wb') as token:
+#             pickle.dump(creds, token)
+#     return creds
 
-def write_dict_to_sheet(sheet_id, data):
+
+def find_zeros_in_column(sheet_id, keys_to_exclude=[]):
     creds = get_credentials()
     service = build('sheets', 'v4', credentials=creds)
 
-    # Clear the sheet
-    clear_request = {"range": "A2:B"}
-    service.spreadsheets().values().clear(spreadsheetId=sheet_id, range="A2:B", body=clear_request).execute()
+    # Read values from column A (index 0)
+    column_A_range_name = 'A1:A1000' # Change 1000 to the number of rows you want to check
+    column_A_result = service.spreadsheets().values().get(spreadsheetId=sheet_id, range=column_A_range_name).execute()
+    column_A_values = column_A_result.get('values', [])
+
+    # Read values from column B (index 1) where you're looking for zeros
+    column_B_range_name = 'B1:B1000'
+    column_B_result = service.spreadsheets().values().get(spreadsheetId=sheet_id, range=column_B_range_name).execute()
+    column_B_values = column_B_result.get('values', [])
+
+    # Combine the values from columns A and B into a dictionary where values from B are "0"
+    combined_dict = {column_A_values[index][0]: value[0] for index, value in enumerate(column_B_values) if value and value[0] == "0" and index < len(column_A_values) and column_A_values[index]}
+
+    # Exclude specified keys from the dictionary
+    for key in keys_to_exclude:
+        combined_dict.pop(key, None)
+
+    return combined_dict
+
+
+def write_dict_to_sheet(sheet_id, data, range_start):
+    creds = get_credentials()
+    service = build('sheets', 'v4', credentials=creds)
+
+    # # Clear the sheet
+    # clear_request = {"range": "A2:B"}
+    # service.spreadsheets().values().clear(spreadsheetId=sheet_id, range="A2:B", body=clear_request).execute()
 
     # Write the data to the sheet
     values = [[k, v] for k, v in data.items()]
     body = {"values": values}
     result = service.spreadsheets().values().update(
-        spreadsheetId=sheet_id, range="A2", valueInputOption="RAW", body=body
+        spreadsheetId=sheet_id, range=range_start, valueInputOption="USER_ENTERED", body=body
     ).execute()
     print(f"{result.get('updatedCells')} cells updated.")
+
+
+if __name__ == "__main__":
+    test = find_zeros_in_column("17bUzAyp989BH3mFF3MsR_ehwHxALi1SW8kAB4y7ZFN4")
+    test = list(test.keys())
+    print(test)
+    test_str = "\n".join(test)
+    print(test_str)
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import base64
+import json
 import requests
 from datetime import datetime
 from google_api_functions import *
+from send_email import send_email
 from keys import *
 
 sheet_id = "17bUzAyp989BH3mFF3MsR_ehwHxALi1SW8kAB4y7ZFN4"  # Replace with your Google Sheet ID
 
-today = datetime.now()
-month = today.month
+# Use this list to exclude any clients that are exceptions to normal contact collection.
+clients_to_exclude = ["Resources for Results (Grant Hensel Test Account - Not Billed)"]
+
+date = datetime.now()
+month = date.month
+today = date.today()
 
 
 #  Get Authorization Token
@@ -55,6 +62,37 @@ def get_activity_report(token, month):
             print(json)
             name = json.get('Customer', 'Key not found')
             visitors = json.get('Visitors_Paid', 'Key not found')
+            data.update({f"{name}":f"{int(visitors)}"})
+    else:
+        print("Status code:", response.status_code)
+        print("Response content:", response.text)
+    return data
+
+
+# Kevin suggested that to get all-time data, I run a series of monthly queries and add them together. #TODO
+def get_all_time_activity_report(token, today):
+    url = "https://api.id-visitors.com/ws/ConsumerReporting/GetConsumerCustomerActivityReport?Date_End=" \
+          + f"{today}" + "&Date_Start=" + "03/01/2023"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+    query_params = {
+        "Date_End": f"{today}",
+        "Date_Start": f"03/01/2023"
+    }
+
+    response = requests.get(url, headers=headers, params=query_params)
+
+    if response.status_code == 200:
+        response_json = response.json()
+        print(response_json)
+        # Iterate through each JSON, grabbing the client name and Visitors Paid
+        data = dict()
+        for json in response_json:
+            print(json)
+            name = json.get('Customer', 'Key not found')
+            visitors = json.get('Visitors_Paid', 'Key not found')
             data.update({f"{name}":f"{visitors}"})
     else:
         print("Status code:", response.status_code)
@@ -62,9 +100,42 @@ def get_activity_report(token, month):
     return data
 
 
-# Runs function to get authentication token.
-token = get_auth_token()
+def get_daily_data(request):
 
-data = get_activity_report(token, month)
+    token = get_auth_token()
+    data = get_activity_report(token, month)
+    write_dict_to_sheet(sheet_id, data, "A2")  # Writes Data For Current Month to "A" Column.
 
-write_dict_to_sheet(sheet_id, data)
+    data = get_activity_report(token, month - 1)
+    write_dict_to_sheet(sheet_id, data, "E2") # Writes Data For Last Month to "E" Column
+
+    data = get_all_time_activity_report(token, today)
+    write_dict_to_sheet(sheet_id, data, "H2")
+
+    return 'Success!', 204
+
+
+    # if request.method == 'POST':
+    #     envelope = json.loads(request.data.decode('utf-8'))
+    #     payload = base64.b64decode(envelope['message']['data']).decode('utf-8')
+    #     print(f"Payload: {payload}")
+
+    # Put functions here.
+
+    #     return '', 204
+    #
+    # else:
+    #     return 'Method not allowed', 405
+
+if __name__ == "__main__":
+    try:
+        get_daily_data("")
+        no_contacts = list(find_zeros_in_column(sheet_id, clients_to_exclude).keys())
+        no_contacts_str = "\n".join(no_contacts)
+        if len(no_contacts) >= 1:
+            send_email("Clients With 0 Collected Contacts", "Hello,\n The following clients have not collected any "
+                                                            "contacts this month and should be investigated: \n\n"
+                                                            f"{no_contacts_str}")
+
+    except:
+        send_email("Error in VV API Data Gatherer", "See Subject")
